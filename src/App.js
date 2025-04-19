8d36e3c44ce93dff6acaf840834a0c5cae75e888
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-19 10:58:30 +0100

    display order

--- src/App.js
+++ src/App.js
@@ -71,24 +71,60 @@ function ProductCard({ productObj, quantity, onAdd, onIncrease, onDecrease }) {
 
 function ShoppingCart({ cart }) {
   const numOrder = Object.values(cart).reduce((a, c) => a + c, 0);
-  console.log(Object.entries(cart));
+
+  const totalPrice = Object.entries(cart).reduce((total, [id, quantity]) => {
+    const product = productData.find((p) => p.id === Number(id));
+    return total + (product ? quantity * product.price : 0);
+  }, 0);
+
   return (
     <div className="shopping-cart">
       <h2>Your Cart ({numOrder})</h2>
 
       {numOrder === 0 ? (
         <>
-          <img src="/images/illustration-empty-cart.svg" alt="emply cart" />
+          <img src="/images/illustration-empty-cart.svg" alt="empty cart" />
           <p>Your added items will appear here</p>
         </>
       ) : (
-        <ul>
-          {Object.entries(cart).map(([id, quantity]) => {
-            const product = productData.find((p) => p.id === Number(id));
-            console.log(product);
-            return <li key={id}>{product?.name}</li>;
-          })}
-        </ul>
+        <>
+          <ul className="order-container">
+            {Object.entries(cart).map(([id]) => {
+              const product = productData.find((p) => p.id === Number(id));
+
+              return (
+                <li key={id}>
+                  <div className="order-details">
+                    <p className="order-name">{product?.name}</p>
+                    <p>
+                      <span className="quantity">{cart[id]}x</span>
+                      <span className="unit-price">
+                        @ ${product?.price.toFixed(2)}
+                      </span>
+                      <span className="unit-total">
+                        ${(cart[id] * product?.price).toFixed(2)}
+                      </span>
+                    </p>
+                  </div>
+                  <button className="btn-delete">&times;</button>
+                </li>
+              );
+            })}
+          </ul>
+
+          <div className="order-total">
+            <p>Order Total</p>
+            <h2>${totalPrice.toFixed(2)}</h2>
+          </div>
+
+          <p className="delivery-info">
+            <img src="/images/icon-carbon-neutral.svg" alt="carbon-neutral" />
+            <span>
+              This is a <strong>Carbon-neutral</strong> delivery
+            </span>
+          </p>
+          <button className="btn-comfirm-order">Confirm Order</button>
+        </>
       )}
     </div>
   );
